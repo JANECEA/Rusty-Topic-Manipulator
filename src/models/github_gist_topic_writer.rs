4004d97bc66d3a4ab91827b074c9358f5fa65c62
JANECEA <adam-janecek@seznam.cz>
2025-01-12 03:56:34 +0100

    Fix banner retrieval

--- src/models/github_gist_topic_writer.rs
+++ src/models/github_gist_topic_writer.rs
@@ -2,7 +2,7 @@ use crate::{
     models::TopicWriter,
     settings::{BannerColor, List},
 };
-use anyhow::anyhow;
+use anyhow::{anyhow, Result};
 use reqwest::blocking::Client;
 use serde_json::json;
 
@@ -58,32 +58,12 @@ impl TopicWriter for GithubGistTopicWriter {
     fn check_source_exist(&self) {}
 
     fn read_list(&mut self) -> anyhow::Result<Vec<String>> {
-        let response = self
-            .client
-            .get(format!("{GITHUB_API_PREFIX}/{}", self.gist_id))
-            .header("Authorization", format!("token {}", self.token))
-            .header("Accept", GITHUB_API_HEADER)
-            .header("User-Agent", "rust-gist-reader")
-            .send()?;
-
-        if !response.status().is_success() {
-            return Err(anyhow!(format!(
-                "Failed to read gist: HTTP {}",
-                response.status()
-            )));
-        }
-
-        let gist_data: serde_json::Value = response.json()?;
-        if let Some(file_content) = gist_data["files"]
-            .get(&self.file_name)
-            .and_then(|file| file["content"].as_str())
-        {
-            let list: Vec<String> = file_content.lines().map(|line| line.to_string()).collect();
-            self.old_list = Some(list.clone());
-            Ok(list)
-        } else {
-            Err(anyhow!("File not found in gist response"))
-        }
+        self.read_gist(&self.gist_id, &self.file_name)
+            .map(|content| {
+                let list: Vec<String> = content.lines().map(|line| line.to_string()).collect();
+                self.old_list = Some(list.clone());
+                Ok(list)
+            })?
     }
 
     fn get_banner(&self) -> &str {
@@ -97,20 +77,22 @@ impl TopicWriter for GithubGistTopicWriter {
 
 impl GithubGistTopicWriter {
     pub fn new(list: &List) -> Self {
+        let client = Client::new();
         let (gist_id, file_name) =
             Self::parse_gist_url(list.path()).expect("Incorrect gist url format.");
 
-        let client = Client::new();
-
-        Self {
+        let mut writer = Self {
             token: list.access_token().to_string(),
             old_list: None,
             gist_id,
             file_name,
-            banner: Self::fetch_banner(&client, list.banner_path()),
+            banner: String::default(),
             banner_color: list.banner_color().clone(),
             client,
-        }
+        };
+
+        writer.banner = writer.fetch_banner(list.banner_path());
+        writer
     }
 
     fn parse_gist_url(url: &str) -> Option<(String, String)> {
@@ -129,10 +111,38 @@ impl GithubGistTopicWriter {
         Some((gist_id.to_string(), file_name.to_string()))
     }
 
-    fn fetch_banner(client: &Client, banner_url: &str) -> String {
-        match client.get(banner_url).send() {
-            Ok(response) => response.text().unwrap_or_default(),
-            Err(_) => String::new(),
+    fn read_gist(&self, gist_id: &str, file_name: &str) -> anyhow::Result<String> {
+        let response = self
+            .client
+            .get(format!("{GITHUB_API_PREFIX}/{}", gist_id))
+            .header("Authorization", format!("token {}", &self.token))
+            .header("Accept", GITHUB_API_HEADER)
+            .header("User-Agent", "rust-gist-reader")
+            .send()?;
+
+        if !response.status().is_success() {
+            return Err(anyhow!(format!(
+                "Failed to read gist: HTTP {}",
+                response.status()
+            )));
         }
+
+        let gist_data: serde_json::Value = response.json()?;
+        match gist_data["files"]
+            .get(file_name)
+            .and_then(|file| file["content"].as_str())
+        {
+            Some(content) => Ok(content.to_string()),
+            None => Err(anyhow!("Could not read gist.")),
+        }
+    }
+
+    fn fetch_banner(&self, banner_url: &str) -> String {
+        let Some((gist_id, gist_file_name)) = Self::parse_gist_url(banner_url) else {
+            return String::default();
+        };
+
+        self.read_gist(&gist_id, &gist_file_name)
+            .unwrap_or_default()
     }
 }
